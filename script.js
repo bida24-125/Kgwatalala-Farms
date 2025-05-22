3f205d2639dba142b00ec4deed98eed8ab162f4a
bida24-125 <bida24-125@thuto.bac.ac.bw>
2025-05-22 11:04:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,18 @@ document.addEventListener("DOMContentLoaded", function () {
     window.location.href = "thanks.html";
   });
 });
+
+/*active navigation page highlight*/
+document.addEventListener("DOMContentLoaded", function () {
+  const currentPage = window.location.pathname.split("/").pop(); 
+  const navLinks = document.querySelectorAll(".navbar a");
+
+  navLinks.forEach(link => {
+    const linkPage = link.getAttribute("href");
+
+    if (linkPage === currentPage) {
+      link.classList.add("active");
+    }
+  });
+});
+
